6899e1d7c5b07e7fe079e4cb39b11b9ee9cf10a4
hilmanzu <hilmanzutech@gmail.com>
2024-12-30 11:15:01 +0800

    feat:

--- components/headers/Header1.jsx
+++ components/headers/Header1.jsx
@@ -49,7 +49,7 @@ export default function Header1({ links }) {
           {/* <LanguageSelect /> */}
           {/* End Languages */}
           <li>
-            <a href="https://abigold.co.id/login" blank className="opacity-1 no-hover">
+            <a href="https://abigold.co.id/login" className="opacity-1 no-hover">
               <span className="link-hover-anim underline" data-link-animate="y">
                 Abi Panel
               </span>
